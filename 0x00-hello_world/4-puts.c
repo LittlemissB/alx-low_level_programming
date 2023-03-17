c1d946d02614200b7eacf0f945a9aaf106337490
Barakat Muyideen <barakatmuyideen@gmail.com>
2023-03-16 19:13:52 -0700

    4 HELLO puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main - the main function 
+ * main - this is the main function
  *
- *  Return : 0 (successful)
- *  */
+ * Return: 0 (successful)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-	
 }
