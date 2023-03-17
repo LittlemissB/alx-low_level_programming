4da1402219e223e1557579a31ad35d885f5f8eeb
Barakat Muyideen <barakatmuyideen@gmail.com>
2023-03-16 21:38:20 -0700

    UNIX simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	char text[] = "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n";
+	char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	ssize_t len = sizeof(text) - 1;
 	ssize_t ret = write(STDERR_FILENO, text, len);
 
