f6f076fc83b9601718e9f46eb5f9a157d0ca1c37
LittlemissB <barakatmuyideen@gmail.com>
2023-03-19 17:22:44 +0100

    Alphabet in uppercase and then in uppercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,17 +8,27 @@
 
 int main(void)
 {
-	char l_alpha = 'a';
-	char u_alpha = 'A';
+	int num1, num2;
 
-	while (l_alpha <= 'z')
+	for (num1 = 0; num1 <  100; num1++) /** Ignore 9 as 90 and 09 are same */
 	{
-		putchar(l_alpha++);
-	}
+		for (num2 = num1; num2 <  100; num2++)
+		{
+			if (num1 < num2)
+			{
+				putchar(48 + (num1 / 10));
+				putchar(48 + (num1 % 10));
+				putchar(' ');
+				putchar(48 + (num2 / 10));
+				putchar(48 + (num2 % 10));
 
-	while (u_alpha <= 'Z')
-	{
-		putchar(u_alpha++);
+				if (num1 != 98 || num2 != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
 	}
 
 	putchar('\n');
