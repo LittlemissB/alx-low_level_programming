d3c3362aa1bb930ea454b05aabd76b84a8694b9c
Barakat Muyideen <barakatmuyideen@gmail.com>
2023-04-05 14:41:33 -0700

    The beautiful string with a new line

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,16 +1,15 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
 
-#include <stdio.h>
-
+int _putchar(char c);
 void _puts_recursion(char *s);
-int_putchar(char c);
-void_print_rev_recursion(char *s);
-int_strlen_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
 int factorial(int n);
-int_pow_recursion(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 
-#endif
+#endif /* MAIN_H_INCLUDED */
