bc3f962c51a37a8982092f60fb3e17aecc07a2b2
Barakat Muyideen <barakatmuyideen@gmail.com>
2023-04-06 05:27:13 -0700

    Is int a prime number or not

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * is_prime_number - tells if an integer is a prime number or otherwise
+ * @n: number to evaluate
+ * @c: iterator
+ * Return: 1 if n is a prime numer, 0 if its not
+ */
+int is_prime_number(unsigned int n, unsigned int c)
+{
+	if (n % c == 0)
+	{
+		if (n == c)
+			return (1);
+		else
+			return (0);
+	}
+	return (0 + is_prime_number(n, c + 1));
+}
+/**
+ * is_prime_number - detects if an input is a prime number
+ * @n: number to evaluate
+ * Return: 1 if n is a prime number, 0 if n is not
+ */
+int is_prime_number(int n)
+{
+	if (n == 0)
+		return (0);
+	if (n < 0)
+		return (0);
+	if (n == 1)
+		return (0);
+	return (is_prime(n, 2));
+}
