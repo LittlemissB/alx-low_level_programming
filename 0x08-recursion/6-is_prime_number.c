11dd0ec65583ecf73ac61fe231fee93ee6e3741e
Barakat Muyideen <barakatmuyideen@gmail.com>
2023-04-06 17:48:45 -0700

    what are prime numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,34 +1,33 @@
 #include "main.h"
 #include <stdio.h>
 
+int _check_prime(int, int i);
+
 /**
  * is_prime_number - tells if an integer is a prime number or otherwise
  * @n: number to evaluate
- * @c: iterator
  * Return: 1 if n is a prime number, 0 if its not
- */
-int is_prime_number( int n, int c)
-{
-	{
-		if (n <= c)
-			return (1);
-		else
-			return (0);
-	}
-	return (is_prime_number(n,  1));
-}
-/**
- * is_prime_number - detects if an input is a prime number
- * @n: number to evaluate
- * Return: 1 if n is a prime number, 0 if n is not
  */
 int is_prime_number(int n)
 {
-	if (n == 0)
+	if (n <= 1)
 		return (0);
-	if (n < 0)
+
+	return (_check_prime(n, n - 1));
+}
+/**
+ * _check_prime -  calculates if a number is prime recursive
+ * @n: number to evaluate
+ * @i: iterator
+ * Return: 1 if n is a prime number, 0 if n is not
+ */
+int _check_prime(int n, int i)
+{
+	if (i == 1)
+		return (1);
+
+	if (n % i == 0 && i > 0)
 		return (0);
-	if (n == 1)
-		return (0);
-	return (is_prime_number(n, 1));
+
+	return (_check_prime(n, i - 1));
 }
