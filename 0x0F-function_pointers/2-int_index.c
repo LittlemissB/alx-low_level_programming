e5de3d6bb10015408b77ee1a4565578c5ca0c92a
Barakat Muyideen <barakatmuyideen@gmail.com>
2023-04-19 16:18:09 -0700

    A lost integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/*
+ * int_index -Searches for an integer via cmp function
+ * @array: Array to search in
+ * @size: Size of the array
+ * @cmp: Pointer to the comparing function
+ * Return: Index of the first element for which
+ * the cmp function does not return 0, or -1 if no match is found
+ * or size is negative
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	if (array && cmp)
+		for (i = 0; i < size; i++)
+			if (cmp(array[i]))
+				return (i);
+	return (-1);
+}
