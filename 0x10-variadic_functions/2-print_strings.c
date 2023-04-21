27a0178b7d18105668fefb91fd1c2a56e6d7bb87
Barakat Muyideen <barakatmuyideen@gmail.com>
2023-04-20 18:45:04 -0700

    lets print numbbers

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_strings - Prints all arguments
+ * @separator: String to separate arguments
+ * @n: Number of arguments
+ *
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		char *v = va_arg(args, char *);
+
+		if (v == NULL)
+			printf("(nil)");
+		else
+			printf("%s", v);
+
+		if (i < (n - 1) && separator)
+			printf("%s", separator);
+	}
+
+	printf("\n");
+	va_end(args);
+}
