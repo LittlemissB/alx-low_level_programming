ee8876bc0bf39dba01cae7aea9968caaf8d7aa7b
Barakat Muyideen <barakatmuyideen@gmail.com>
2023-04-27 21:50:18 -0700

    Le liere et la tortue

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * construc_func - Run code first
+ */
+void __attribute__((constructor)) construc_func(void)
+{
+	printf("You're beat! and yet, you must allow, \n");
+	printf("I bore my house upon my back!n");
+}
---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - Fress the linked list memory
+ * @head: Head Node
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+
+		free(temp->str);
+		free(temp);
+	}
+}
+
