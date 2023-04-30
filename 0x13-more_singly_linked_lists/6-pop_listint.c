3df2cf641bc4da8b9e49193ef6ca8e84a9846e64
Barakat Muyideen <barakatmuyideen@gmail.com>
2023-04-30 04:47:02 -0700

    Delectes the head node

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * pop_listint - Deletes the head of the list
+ * @head: Head Node
+ * Return: The data inside the elements that was deleted
+ * or 0 if the list  is empty
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int data;
+
+	if (!*head || !head)
+		return (0);
+
+	data = (*head)->n;
+	temp = *head;
+	*head = (*head)->next;
+
+	free(temp);
+	return (data);
+}
