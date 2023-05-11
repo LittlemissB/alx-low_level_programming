6d591f9fed2d8fe6ce262e7524d904c1452a9f2a
Barakat Muyideen <barakatmuyideen@gmail.com>
2023-05-11 14:38:05 -0700

    File creation

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,9 +13,9 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	file = open(filename, 0_WRONLY | 0_CREAT | 0_TRUNC, S_IRUSR | S_IWUSR);
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	bytesWritten = write(file, text_content, len);
 
 	close(file);
 	return ((file == -1 || bytesWritten == -1) ? -1 : 1);
-i}
+}
